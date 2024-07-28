93a8d8a7bf46ef89d737609387be95276d7b6ff1
ilovrec <iva.lovrec@tacta.io>
2024-07-28 18:55:46 +0200

    Setup Spring boot app with first endpoint

---
+++ src/test/java/com/laleco/LalecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laleco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalecoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
